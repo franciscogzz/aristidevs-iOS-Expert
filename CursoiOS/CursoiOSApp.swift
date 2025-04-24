c3b9d4f45932b36ee3abb5266f44c3535a537d58
Francisco Gonzalez <franciscogzz@proton.me>
2025-04-24 08:36:54 -0600

    Initial Commit

---
+++ CursoiOS/CursoiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  CursoiOSApp.swift
+//  CursoiOS
+//
+//  Created by Francisco Gonzalez on 24/04/25.
+//
+
+import SwiftUI
+
+@main
+struct CursoiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CursoiOSTests/CursoiOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  CursoiOSTests.swift
+//  CursoiOSTests
+//
+//  Created by Francisco Gonzalez on 24/04/25.
+//
+
+import Testing
+@testable import CursoiOS
+
+struct CursoiOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
