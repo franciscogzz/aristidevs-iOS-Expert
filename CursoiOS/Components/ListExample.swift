6d796235696cb8e560d3c865df5d1fc91276c2e9
Francisco Gonzalez <12665005+franciscogzz@users.noreply.github.com>
2025-05-13 08:57:22 -0600

    IMC terminado y ejemplo de listas

---
+++ CursoiOS/Components/ListExample.swift
@@ -0,0 +1,18 @@
+//
+//  ListExample.swift
+//  CursoiOS
+//
+//  Created by Francisco Gonzalez on 13/05/25.
+//
+
+import SwiftUI
+
+struct ListExample: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListExample()
+}
---
+++ CursoiOS/IMC/IMCResultView.swift
@@ -0,0 +1,18 @@
+//
+//  IMCResultView.swift
+//  CursoiOS
+//
+//  Created by Francisco Gonzalez on 12/05/25.
+//
+
+import SwiftUI
+
+struct IMCResultView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IMCResultView()
+}
